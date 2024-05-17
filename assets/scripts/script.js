a42ddb86150a29cacaa0b451d15ac163dfcb4b5d
Rafilksz <Rafael.longas1989@gmail.com>
2024-05-16 23:30:09 -0300

    create new br albums

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,54 +1,94 @@
-function changeLanguage(select) {
-    var selectedOption = select.options[select.selectedIndex].value;
-    var currentPath = window.location.pathname;
+var currentImageIndex = 0;
+var images = [
+  "/assets/img/album_1.png",
+  "/assets/img/album_2.png",
+  "/assets/img/album_3.png",
+  "/assets/img/album_4.png",
+  "/assets/img/album_5.png",
+  "/assets/img/album_6.png"
+];
 
-    var paths = {
-        "/index.html": "/pages/brindex.html",
-        "/pages/brindex.html": "/index.html",
-        "/pages/encontact.html": "/pages/brcontact.html",
-        "/pages/brcontact.html": "/pages/encontact.html",
-        "/pages/enabout.html": "/pages/brabout.html",
-        "/pages/brabout.html": "/pages/enabout.html"
-    };
-
-    // Desabilitar a opção apropriada
-    if (currentPath === "/index.html" || currentPath === "/pages/encontact.html" || currentPath === "/pages/enabout.html") {
-        select.options[1].disabled = false; // Desabilita "EN"
-        select.options[2].disabled = true; // Habilita "PT"
-    } else if (currentPath === "/pages/brindex.html" || currentPath === "/pages/brcontact.html" || currentPath === "/pages/brabout.html") {
-        select.options[1].disabled = true; // Habilita "EN"
-        select.options[2].disabled = false; // Desabilita "PT"
-    }
-
-    // Redirecionar para a página apropriada
-    if (selectedOption === "pt-br" && paths[currentPath]) {
-        window.location.href = paths[currentPath];
-    } else if (selectedOption === "en-us" && paths[currentPath]) {
-        window.location.href = paths[currentPath];
-    }
+function openLightbox(index) {
+  currentImageIndex = index;
+  document.getElementById("lightbox-img").src = images[index];
+  document.getElementById("lightbox").style.display = "flex";
+  document.addEventListener("keydown", keyboardNavigation);
 }
 
-// Executar a função ao carregar a página para definir a opção correta como desabilitada
-window.onload = function() {
-    var languageSelect = document.getElementById("language-select");
-    changeLanguage(languageSelect);
-};
+function closeLightbox(event) {
+  if (event.target === event.currentTarget || event.target.classList.contains('close')) {
+    document.getElementById("lightbox").style.display = "none";
+    document.removeEventListener("keydown", keyboardNavigation);
+  }
+}
 
+function changeImage(direction) {
+  currentImageIndex += direction;
+  if (currentImageIndex < 0) {
+    currentImageIndex = images.length - 1;
+  } else if (currentImageIndex >= images.length) {
+    currentImageIndex = 0;
+  }
+  document.getElementById("lightbox-img").src = images[currentImageIndex];
+}
+
+function keyboardNavigation(event) {
+  if (event.key === "ArrowLeft") {
+    changeImage(-1);
+  } else if (event.key === "ArrowRight") {
+    changeImage(1);
+  } else if (event.key === "Escape") {
+    closeLightbox({target: document.getElementById("lightbox")});
+  }
+}
+
+// EmailJS initialization
+(function() {
+  emailjs.init("YOUR_USER_ID");
+})();
+
+function changeLanguage(select) {
+  var selectedOption = select.options[select.selectedIndex].value;
+  var currentPath = window.location.pathname;
+
+  var paths = {
+    "/index.html": "/pages/brindex.html",
+    "/pages/brindex.html": "/index.html",
+    "/pages/encontact.html": "/pages/brcontact.html",
+    "/pages/brcontact.html": "/pages/encontact.html",
+    "/pages/enabout.html": "/pages/brabout.html",
+    "/pages/brabout.html": "/pages/enabout.html"
+  };
+
+  if (selectedOption === "pt-br" && paths[currentPath]) {
+    window.location.href = paths[currentPath];
+  } else if (selectedOption === "en-us" && paths[currentPath]) {
+    window.location.href = paths[currentPath];
+  }
+}
 
 function sendEmail() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
 
-    var mailtoLink = "mailto:rafael.longas1989@gmail.com?subject=Contato&body=" +
-        "Nome: " + name + "%0D%0A" +
-        "Email: " + email + "%0D%0A" +
-        "Mensagem: " + message;
+  var templateParams = {
+    from_name: name,
+    from_email: email,
+    message: message
+  };
 
-    window.location.href = mailtoLink;
+  emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', templateParams)
+    .then(function(response) {
+      console.log('SUCCESS!', response.status, response.text);
+      alert('Mensagem enviada com sucesso!');
+    }, function(error) {
+      console.log('FAILED...', error);
+      alert('Ocorreu um erro ao enviar a mensagem.');
+    });
 }
 
 window.onload = function() {
-    var languageSelect = document.getElementById("language-select");
-    changeLanguage(languageSelect);
+  var languageSelect = document.getElementById("language-select");
+  changeLanguage(languageSelect);
 };
